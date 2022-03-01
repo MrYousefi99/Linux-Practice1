367defbec3a20e26545a5a2de2070d8b697c0769
Amir Hossein Yousefi <84061517+MrYousefi99@users.noreply.github.com>
2022-03-01 12:12:35 +0330

    Add files via upload

---
+++ Test1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+
+amirhosseinyousefi@MrDeveloperP:~$ arch
+x86_64
+amirhosseinyousefi@MrDeveloperP:~$ uptime
+ 11:23:50 up 29 min,  1 user,  load average: 0.29, 0.32, 0.20
+amirhosseinyousefi@MrDeveloperP:~$ free
+              total        used        free      shared  buff/cache   available
+Mem:        3982616      953320     2300284        2368      729012     2797536
+Swap:        945416           0      945416
+amirhosseinyousefi@MrDeveloperP:~$ du Documents
+4	Documents
+
---
+++ Test2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+amirhosseinyousefi@MrDeveloperP:~$ mkdir newfolder
+amirhosseinyousefi@MrDeveloperP:~$ cd 
+amirhosseinyousefi@MrDeveloperP:~$ cd newfolder
+amirhosseinyousefi@MrDeveloperP:~/newfolder$ touch txt.txt
+amirhosseinyousefi@MrDeveloperP:~/newfolder$ touch test.py
+amirhosseinyousefi@MrDeveloperP:~/newfolder$ touch file.txt
+amirhosseinyousefi@MrDeveloperP:~/newfolder$ cd
+amirhosseinyousefi@MrDeveloperP:~$ rm -r newfolder
+amirhosseinyousefi@MrDeveloperP:~$
